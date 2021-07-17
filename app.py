da57a3c91c1ede5fb23ff7de226abab5f7ccd921
Jack Arthur Harrhy <me@jackharrhy.com>
2021-07-17 14:09:34 -0230

    rainbow flask, #1

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import random
 from flask import Flask
 
 app = Flask(__name__)
+
+def random_color():
+    return "#{:06x}".format(random.randint(0, 0xFFFFFF))
 
 @app.route("/")
 def route_hello_world():
@@ -9,3 +13,18 @@ def route_hello_world():
 @app.route("/<name>")
 def route_hello_name(name):
     return f"<p>Hello, {name}!</p>"
+
+@app.route("/rainbow")
+def rainbow():
+    return f"""
+<head>
+    <style>
+        body {{
+            background-color: {random_color()};
+        }}
+    </style>
+</head>
+<body>
+    <p>Rainbow Flask</p>
+</body>
+    """
